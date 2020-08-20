9459d82962055fb87bcc24fa4dde011a347f773e
Diego Elgart <diegojelgart@gmail.com>
2020-08-20 11:31:25 +0300

    5: bootsrap-install and configure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <p className='text-danger'>Bootstrap works !!</p>
+      <i className=' fas fa-user'></i>Fontawesome works!
+    </div>
+  );
 }
 
 export default App;
